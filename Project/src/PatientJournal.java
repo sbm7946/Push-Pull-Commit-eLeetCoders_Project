d768535a7ac1827a9d683421fec56c437caed4a3
Siena <sbullockmartin7946@sdsu.edu>
2026-03-04 15:20:49 -0800

    Final Proj Set Up

--- Project/src/PatientJournal.java
+++ Project/src/PatientJournal.java
@@ -5,11 +5,11 @@ import java.util.Objects;
 public class PatientJournal {
     final String name = "Beth";
     final int age = 68;
-    static String report;
-    static String date;
-    static String time;
-    static boolean worried;
-    static ArrayList<PatientJournal> patientJournal = new ArrayList();
+    String report;
+    String date;
+    String time;
+    boolean worried;
+
 
     public PatientJournal(String date1, String time1, boolean worried1, String report1) {
         date = date1;
@@ -18,43 +18,38 @@ public class PatientJournal {
         report = report1;
     }
 
-    public static void updateReport(String date1, String time1, boolean worried1, String report1) {
+    public static void updateReport(ArrayList<PatientJournal> journal, String date1, String time1, boolean worried1, String report1) {
         PatientJournal report = new PatientJournal(date1, time1, worried1, report1);
-        patientJournal.add(report);
+        journal.add(report);
     }
 
-    public static void getReport(int n) {
-        int length = patientJournal.size();
+    public static void getReport(int n, ArrayList<PatientJournal> journal) {
+        int length = journal.size();
 
-        for(int i = length - 1; i >= length - n; --i) {
+        for(int i = length - 1; i >= length - n; i--) {
             System.out.println();
-            PrintStream var10000 = System.out;
-            Objects.requireNonNull((PatientJournal)patientJournal.get(i));
-            Objects.requireNonNull((PatientJournal)patientJournal.get(i));
-            PatientJournal var10003 = (PatientJournal)patientJournal.get(i);
-            String var6 = date;
-            PatientJournal var10004 = (PatientJournal)patientJournal.get(i);
-            String var7 = date;
-            PatientJournal var10005 = (PatientJournal)patientJournal.get(i);
-            var10000.println("Report for " + "Beth" + " aged " + 68 + " on " + var6 + " at " + var7 + " is " + time);
-            var10000 = System.out;
-            PatientJournal var10001 = (PatientJournal)patientJournal.get(i);
-            var10000.println("    Worried: " + worried);
-            var10000 = System.out;
-            var10001 = (PatientJournal)patientJournal.get(i);
-            var10000.println("    Report: " + report);
-            System.out.println();
+            String repName  = journal.get(i).name;
+            int repAge = journal.get(i).age;
+            String repDate = journal.get(i).date;
+            String repTime = journal.get(i).time;
+            boolean repWorried = journal.get(i).worried;
+            String repReport = journal.get(i).report;
+
+            System.out.println("Report for " + repName + " aged " + repAge + " on " + repDate + " at " + repTime);
+            System.out.println("    Worried: " + repWorried);
+            System.out.println("    Report: " + repReport);
         }
 
     }
 
     public static void main(String[] args) {
+        ArrayList<PatientJournal> beths_journal = new ArrayList();
         boolean log = true;
         if (log) {
-            updateReport("March 3rd", "17:24", false, "Was very active today");
-            updateReport("March 4th", "18:00", true, "Took a fall at 12pm. Medical team says to monitor closely");
+            updateReport(beths_journal, "March 3rd", "17:24", false, "Was very active today");
+            updateReport(beths_journal, "March 4th", "18:00", true, "Took a fall at 12pm. Medical team says to monitor closely");
         }
 
-        getReport(2);
+        getReport(2, beths_journal);
     }
 }
