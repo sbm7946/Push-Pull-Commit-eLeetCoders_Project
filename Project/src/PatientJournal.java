d85316ed91a0efb395081e5f89e1a3ca978cccb9
sbm7496 <sbullockmartin7946@sdsu.edu>
2026-03-10 10:46:49 -0700

    tester

--- Project/src/PatientJournal.java
+++ Project/src/PatientJournal.java
@@ -57,6 +57,7 @@ public class PatientJournal {
 
         }
         System.out.println("Hi eleet Coders!!!");
+        System.out.println("This was written in VS Code");
         getReport(3, beths_journal);
     }
 }
